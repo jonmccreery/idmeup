59fba3b90c9afb8cd564718b022f413cba450748
taxi <jmccreery@gmail.com>
2024-07-27 20:33:11 -0400

    tweak ansible template

--- terraform/export.tf
+++ terraform/export.tf
@@ -1,6 +1,6 @@
 resource "local_file" "ansible_ini_file" {
   content  = <<-EOF
-  [nodes]
+[nodes]
 app
 http
 db
@@ -20,7 +20,6 @@ monitor
 
 [nodes:vars]
 ansible_ssh_common_args='-o StrictHostKeyChecking=no -o ProxyCommand="ssh -p 22 -W %h:%p -q -i ../../secret/ansible ansible@${google_compute_instance.bastion.network_interface[0].access_config[0].nat_ip}"'
-#ansible_ssh_common_args='-o StrictHostKeyChecking=no -o ProxyCommand="ssh -p 22 -W %h:%p -q -i ../../secret/ansible ansible@104.154.171.91"'
 EOF
   filename = "../ansible/idmeup.ini"
 
