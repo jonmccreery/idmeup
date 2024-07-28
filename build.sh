1ec6db23eaaaedb9266f254d7c37c3b6261ac526
taxi <jmccreery@gmail.com>
2024-07-27 20:28:47 -0400

    collect ansible requirements in one file

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ cd terraform
 terraform apply
 
 cd ../ansible
+ansible-galaxy install -r requirements.yml
+
 ansible-playbook --private-key ../../secret/ansible -u ansible -i idmeup.ini http.yml
 ansible-playbook --private-key ../../secret/ansible -u ansible -i idmeup.ini app.yml
-ansible-playbook --private-key ../../secret/ansible -u ansible -i idmeup.ini monitor.yml
 ansible-playbook --private-key ../../secret/ansible -u ansible -i idmeup.ini db.yml
+ansible-playbook --private-key ../../secret/ansible -u ansible -i idmeup.ini monitor.yml
