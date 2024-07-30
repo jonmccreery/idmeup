2debd6ad82500cfddf70f187b18047103595e11f
taxi <jmccreery@gmail.com>
2024-07-30 19:23:48 -0400

    resequence build

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ terraform apply
 cd ../ansible
 ansible-galaxy install -r requirements.yml
 
-ansible-playbook --private-key ../../secret/ansible -u ansible -i idmeup.ini http.yml
-ansible-playbook --private-key ../../secret/ansible -u ansible -i idmeup.ini app.yml
 ansible-playbook --private-key ../../secret/ansible -u ansible -i idmeup.ini db.yml
+ansible-playbook --private-key ../../secret/ansible -u ansible -i idmeup.ini app.yml
+ansible-playbook --private-key ../../secret/ansible -u ansible -i idmeup.ini http.yml
 ansible-playbook --private-key ../../secret/ansible -u ansible -i idmeup.ini monitor.yml
